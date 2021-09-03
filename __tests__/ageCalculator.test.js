ba2562d37b97010e9c92cef0fc606509d2edb7b8
Art Weinstein <artur.weinstein@gmail.com>
2021-09-03 08:35:13 -0700

    create test for a person constructor with an initial name and age.

---
+++ __tests__/ageCalculator.test.js
@@ -0,0 +1,11 @@
+const { exportAllDeclaration } = require("@babel/types");
+const { TestScheduler } = require("jest-cli")
+
+
+describe ('Person', () =>{
+  test('Should create a person object with an initial age', () =>{
+    let person = new Person('Art', 28);
+    expect(person.name).toEqual("Art");
+    expect(person.age).toEqual(28);
+  });
+});
