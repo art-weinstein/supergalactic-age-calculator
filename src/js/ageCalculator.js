6280d10f84412289080b8c15beedf00ce0361cd8
Art Weinstein <artur.weinstein@gmail.com>
2021-09-03 09:11:41 -0700

    create basic function that passes the test for years lived beyond average age.

--- src/js/ageCalculator.js
+++ src/js/ageCalculator.js
@@ -3,8 +3,13 @@ export class Person {
     this.name = name;
     this.age = age;
     this.averageRemaining = 0;
+    this.livedBeyond = 0;
   };
   averageAgeLeft() {
+    if (this.age <= 80){
     this.averageRemaining = (80 - this.age);
-  };
+    } else {
+    this.livedBeyond = (this.age - 80);
+    }
+  }
 };
