3326a9362d58e0a5573cbc4915e9e4594e5d09ed
Art Weinstein <artur.weinstein@gmail.com>
2021-09-03 09:06:18 -0700

    create test to calculate how much a person has lived beyond expected average.

--- __tests__/ageCalculator.test.js
+++ __tests__/ageCalculator.test.js
@@ -10,8 +10,14 @@ describe ('Person', () =>{
   });
   test('Should calculate the rough amount of years a person has left to live, baseline average will be 80', () =>{
     let person = new Person ('Art', 28);
-    expect(person.averageAgeLeft).toEqual(52);
-  })
+    person.averageAgeLeft();
+    expect(person.averageRemaining).toEqual(52);
+  });
+  test('Should calculate years beyond expected average if user has surpassed it', () =>{
+    let person = new Person ('Roger', 90);
+    person.averageAgeLeft();
+    expect(person.livedBeyond).toEqual(10);
+  });
   // test('Should calculate the age of a person on Mercury', () =>{
   //   let person = new Person('Art', 28);
   //   expect(person.mercuryAge).toEqual(116);
--- src/js/ageCalculator.js
+++ src/js/ageCalculator.js
@@ -2,5 +2,9 @@ export class Person {
   constructor(name, age){
     this.name = name;
     this.age = age;
-  }
-}
+    this.averageRemaining = 0;
+  };
+  averageAgeLeft() {
+    this.averageRemaining = (80 - this.age);
+  };
+};
